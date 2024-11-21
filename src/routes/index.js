6c94dc22a2040f6b3d42a9f8d6005e0f196a8674
Joel <jen21092@byui.edu>
2024-11-21 11:13:31 -0700

    configuring swagger

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+// Create a new router object using the express framework
+const router = require("express").Router();
+
+// Set up Swagger subroute
+router.use("/", require("./swagger"));
+
+// Set up other routes 
+router.use("/category", require("./catergoryRoutes"));
+router.use("/item", require("./itemRoutes"));
+router.use("/user", require("./userRoutes"));
+
+// Exports router object 
+module.exports = router;
