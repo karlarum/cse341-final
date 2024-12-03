414b23366ec0075911a5e4fbde9429699e78fc47
Ethan Glenn <ethan+git@eglenn.dev>
2024-12-03 10:40:57 -0700

    Add user routes and session management to server configuration

--- server.js
+++ server.js
@@ -5,18 +5,29 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 const categoryRoutes = require("./routes/category");
 const itemRoutes = require("./routes/itemRoutes");
+const userRoutes = require("./routes/userRoutes");
+const session = require('express-session');
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const deployment = process.env.NODE_ENV || "dev";
+const secretKey = process.env.SECRET_KEY;
 
-// json request are accessible at req.body
 app.use(express.json());
+
+app.use(session({
+  secret: secretKey,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: deployment === 'prod' }
+}));
 
 app
   .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use("/category", categoryRoutes)
   .use("/item", itemRoutes)
+  .use("/user", userRoutes)
   .use("/", require("./src/routes"));
 
 // Connect to MongoDB
