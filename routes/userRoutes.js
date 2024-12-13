8e3e6d0a2ce4fa0d8997892d39547c08ce7f1d0b
Ethan Glenn <ethan+git@eglenn.dev>
2024-12-13 15:30:07 -0700

    Add GitHub OAuth authentication routes to userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userController = require("../controllers/userController");
 const router = express.Router();
 const { ensureAuthenticated } = require("../src/session");
+require("dotenv").config();
 
 router.post("/", userController.createUser);
 router.post("/login", userController.loginUser);
@@ -12,6 +13,11 @@ router.get("/logout", userController.logoutUser);
 router.get("/:username", userController.getUserByUsername);
 router.put("/:username", userController.updateUser);
 router.delete("/:username", userController.deleteUser);
+router.get("/auth", (req, res) => {
+    const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${process.env.GITHUB_REDIRECT_URI}&scope=user`;
+    res.redirect(redirectUrl);
+});
+router.get("/auth/callback", userController.authUserCallback);
 
 
 module.exports = router;
