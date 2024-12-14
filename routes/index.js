086dda6c8fc8aa57533badcfddfdb92952ae772c
Ethan Glenn <ethan+git@eglenn.dev>
2024-12-13 19:31:22 -0700

    Fix user route authentication and cleanup redundant code

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,10 @@ const { ensureAuthenticated } = require("../src/session");
 // router.use("/category", require("./categoryRoutes"));
 // router.use("/item", require("./itemRoutes"));
 // router.use("/user", require("./userRoutes"));
-
+router.use("/user", require("./userRoutes"));
+router.use(ensureAuthenticated);
 router.use("/category", require("./catergoryRoutes"));
 router.use("/item", require("./itemRoutes"));
-router.use("/user", require("./userRoutes"));
 
 // Exports router object
 module.exports = router;
